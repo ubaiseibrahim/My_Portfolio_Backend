52206ca77efd044ca7488a8d8dfebf0ec2ef4fe1
ubaiseibrahim <ubaiseibrahim17@gmail.com>
2025-12-30 12:12:26 +0530

    Changes on config

--- config.php
+++ config.php
@@ -18,6 +18,25 @@ class Database {
         return $this->conn;
     }
 }
-?>
 
 
+// <?php
+// class Database {
+//     private $host = "sql107.infinityfree.com";
+//     private $db_name = "if0_40790704_ubaise_ibrahim";
+//     private $username = "if0_40790704";
+//     private $password = "Q7yOoBA9xUl5I";
+//     public $conn;
+
+//     public function getConnection() {
+//         $this->conn = null;
+//         try {
+//             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+//             $this->conn->exec("set names utf8");
+//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         } catch(PDOException $exception) {
+//             error_log("Connection error: " . $exception->getMessage());
+//         }
+//         return $this->conn;
+//     }
+// }
